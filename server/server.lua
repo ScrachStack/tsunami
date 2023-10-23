a6963d33581dd6212a4c5841fd9e7fdefeb206de
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-23 15:24:01 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,25 @@
 
+
+AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
+    if eventData.secondsRemaining == 900 then
+
+    
+    end
+end)
+
+-- 5 Minutes Reminder
+AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
+    if eventData.secondsRemaining == 300 then
+
+    
+    
+    end
+end)
+
+-- 1 Minutes Reminder
+AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
+    if eventData.secondsRemaining == 80 then
+
+    
+    end
+end)
