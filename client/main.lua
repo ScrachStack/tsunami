9d256381a826fda936d27582e37a2a127a3b2f11
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-24 11:16:23 -0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -2,3 +2,17 @@ RegisterNetEvent('Play:Sound')
 AddEventHandler('Play:Sound', function(soundName, soundVolume)
     TriggerEvent('InteractSound_CL:PlayOnOne', soundName, soundVolume)
 end)
+RegisterNetEvent('changeWeather')
+AddEventHandler('changeWeather', function(weatherType)
+    if IsAllowedToChangeWeather() then
+        SetWeatherTypeNowPersist(weatherType)
+        SetWeatherTypeNow(weatherType)
+        Citizen.Wait(1000) 
+    else
+        TriggerEvent('chatMessage', '^1[Error]^0 You do not have permission to change the weather.')
+    end
+end)
+
+function IsAllowedToChangeWeather()
+    return true
+end
