0e734b18f0be2d7af63933887dfc3f3de044f1c9
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-24 11:18:00 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -9,15 +9,18 @@ AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
     if eventData.secondsRemaining == 900 then
         TriggerEvent('Play:ServerSound', 'Alert', 0.1)
         message = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(255, 0, 0, .9); border-radius: 2px;"><b> [SYSTEM] </b> <i>Tsunami Warning!</i> <br>Time Remaining: 15 minutes</div>'
-
+        TriggerClientEvent('changeWeather', 'THUNDER')
     elseif eventData.secondsRemaining == 300 then
         TriggerEvent('Play:ServerSound', 'Alert', 0.1)
         message = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(255, 0, 0, .9); border-radius: 2px;"><b> [SYSTEM] </b> <i>Tsunami Warning!</i> <br>Time Remaining: 5 minutes</div>'
+        TriggerClientEvent('changeWeather', 'THUNDER')
 
     elseif eventData.secondsRemaining == 80 then
         TriggerEvent('Play:ServerSound', 'Alert', 0.1)
         message = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(255, 0, 0, .9); border-radius: 2px;"><b> [SYSTEM] </b> <i>Tsunami Warning!</i> <br>Time Remaining: 1 minute and 20 seconds</div>'
-    end
+         TriggerClientEvent('changeWeather', 'THUNDER')
+
+        end
           if message ~= "" then
         TriggerClientEvent('chat:addMessage', -1, { template = message })
     end
